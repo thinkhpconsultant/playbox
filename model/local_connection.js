305b19d11f6434321a3bcfdb8f8ddeb7ca75cf13
dhruval-moradiya-au18 <dhruvalmoradiya@gmail.com>
2023-02-28 14:55:43 +0530

    mysql

--- model/local_connection.js
+++ model/local_connection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 var conn = mysql.createConnection({ //Creating a connection with the database
-    host:'sql946.main-hosting.eu',
-    user:'u839608378_thinkhp',
-    password:'ThinkDM001@',
-    database:'u839608378_thinkhp_base'
+    host:'locolhost',
+    user:'root',
+    password:'',
+    database:''
 })
 
 module.exports = conn;
